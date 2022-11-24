d361c94947f110d597ff73076eaa2b2f11c0d25c
zetra <karanpatadia28@gmail.com>
2022-11-24 16:10:24 +0530

    redesign top nav

--- src/components/common/top-nav/top-nav.styles.jsx
+++ src/components/common/top-nav/top-nav.styles.jsx
@@ -3,21 +3,22 @@ import styled from 'styled-components';
 import THEME, { fontStyle } from '../../../theme/theme.styles';
 
 export const TopNavContainer = styled.div`
-  background-color: ${THEME.grey};
+  background-color: ${THEME.black};
   border-radius: 25px;
   margin: 0px 5%;
 
   button {
     ${fontStyle('Roboto-Medium', THEME.text, 'normal', 'center', 16, 16, 0)};
-    background-color: ${THEME.grey};
+    background-color: ${THEME.black};
     width: 50%;
     padding: 20px 0;
-    border-radius: 25px;
+    /* border-radius: 25px; */
     box-shadow: none;
     border: none;
   }
   button.active {
-    background-color: ${THEME.text};
-    color: ${THEME.grey};
+    /* background-color: ${THEME.text};
+    color: ${THEME.grey}; */
+    border-bottom: 1px solid ${THEME.focus};
   }
 `;
